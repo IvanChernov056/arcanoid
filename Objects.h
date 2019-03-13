f9fa64b776dbc264af79ca5b81e675612929f8a7
IvanChernov056 <48418123+IvanChernov056@users.noreply.github.com>
2019-03-14 02:30:27 +0700

    Add files via upload

--- Objects.h
+++ Objects.h
@@ -11,50 +11,46 @@
 
 
 #include    <iostream>
+#include    <fstream>
+#include    <sstream>
 #include    <vector>
 #include    <cmath>
 #include    <algorithm>
 
+class   Object;
+struct  Mesh;
 class   Block;
 class   Player;
 class   Ball;
 
+const float BlockX = 3, BlockY = 1,
+            PlayerX = 10, PlayerY = 1,
+            PlayerVel = 30.f,
+            BallR = 1.0f,
+            VelMag = 40.0f,
+            VelX = VelMag*cos(M_PI_4), VelY = VelMag*sin(M_PI_4);
+bool    keys[1024];
 
 struct  Scene {
 
     GLuint  SizeX, SizeY;
-    GLuint  BlocksRow, BlocksCol;
+    GLuint  BlocksRow{20}, BlocksCol{15};
 
     Player  *player{nullptr};
     Ball    *ball{nullptr};
 
+    Mesh    *blockMesh{nullptr};
+
+    std::vector<Block> blockList;
     GLuint      shaderProgram;
     glm::mat4   view;
 
-    Scene (GLuint sx, GLuint sy, GLuint sh_prog) : SizeX(sx), SizeY(sy), shaderProgram(sh_prog) {
-        BlocksRow = 7;
-        BlocksCol = 15; 
+    Scene (GLuint sx, GLuint sy, GLuint sh_prog, Mesh **m);
 
-        view = glm::translate (glm::mat4(1.0f), glm::vec3 (-1.f, -1.f, 0.f));
-        view = glm::scale (view, glm::vec3 (2.0f/SizeX, 2.0f/SizeY, 1.0f));
-    }
-};
-
-class   Object {
-    public:
-        Scene   *scene{nullptr};
-};
-
-class   PhisicObject : public Object {
-
-    public:
-
-        glm::vec3   pos;
-        glm::vec3   scl;
-
-        virtual void update (float elapsedTime) {
-
-        }
+    void Draw();
+    void update(float elapsedTime);
+    void resetRaund ();
+    void resetGame ();
 };
 
 struct  Mesh {
@@ -172,220 +168,255 @@ struct  Mesh {
         }
 };
 
-
-struct   Perfab : public Object {
-
-    PhisicObject    *obj{nullptr};
-    Mesh            *mesh{nullptr};
-
-    Perfab (PhisicObject *o, Mesh *m) : obj(o), mesh(m) {
-        scene = (obj->scene != nullptr) ? obj->scene : nullptr;
-        std::cout << scene << std::endl;
-    }
-
-    void    Draw () {
-        glBindVertexArray (mesh->VAO);
-
-        glm::mat4   model = glm::translate (glm::mat4 (1.0f), obj->pos);
-        model = glm::scale (model, obj->scl);
-
-        GLuint      viewLoc = glGetUniformLocation (scene->shaderProgram, "view");
-        glUniformMatrix4fv (viewLoc, 1, GL_FALSE, glm::value_ptr(scene->view));
-
-        GLuint     modelLoc = glGetUniformLocation (scene->shaderProgram, "model");
-        glUniformMatrix4fv (modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-
-        mesh->Draw();
-
-        glBindVertexArray (0);
-    }
-};
-
-
-class   Block : public PhisicObject {
-    
-    public:
-        Block (glm::vec2 p, Scene *sc) {
-            scene = sc;
-            pos = glm::vec3 (p, 0.0f);
-            scl = glm::vec3 (3.0f, 1.0f, 0.0f);
-        }
-        ~Block () {}
-
-        void update (float elaepsedTime) {
-            alive = !ballCollision ();
-        }
-
-        bool ballCollision ();
-
-        bool    alive{true};
-};
-
-bool    keys[1024];
-
-class   Player : public PhisicObject {
+class Object {
 
     public:
-        Player (Scene *sc) {
-            scene = sc;
-            scl = glm::vec3 (14.5f, 1.0f, 1.0f);
-            pos = glm::vec3 (scene->SizeX/2.0f, scl.y, 0.0f);
-            scene->player = this;
+        glm::vec3   pos;
+        glm::vec3   scl;
+        Scene       *scene{nullptr};
+        Mesh        *mesh{nullptr};
+
+        Object (Scene *sc, Mesh *m) : scene(sc), mesh(m) {}
+
+        virtual void update (float elapsedTime) {}
+
+        void    Draw () {
+            glBindVertexArray(mesh->VAO);
+
+            glm::mat4   model = glm::translate (glm::mat4(1.0), pos);
+            model = glm::scale (model, scl);
+
+            GLuint  modUnifLoc = glGetUniformLocation (scene->shaderProgram, "model");
+            glUniformMatrix4fv (modUnifLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+            GLuint  viewUnifLoc = glGetUniformLocation (scene->shaderProgram, "view");
+            glUniformMatrix4fv (viewUnifLoc, 1, GL_FALSE, glm::value_ptr(scene->view));
+
+            mesh->Draw();
+
+            glBindVertexArray(0);
         }
-        ~Player () {}
+};
 
-        float   v_mag{100.f};
+enum DIRECTION {LEFT=-1, RIGHT=1};
+class   Player : public Object {
 
-        void update (float elapsedTime) override {
-            move (elapsedTime);
-            ballCollision();
+    public:
+
+        Player (Scene *sc, Mesh *m) : Object (sc, m) {
+            pos = glm::vec3 (scene->SizeX/2, PlayerY/2, 0.0f);
+            scl = glm::vec3 (PlayerX, PlayerY, 0.0f);
         }
 
-        void ballCollision ();
-
-        void move (float elapsedTime) {
-            float xPos = pos.x;
-
+        void    update (float elapsedTime) {
             if (keys[GLFW_KEY_LEFT]) {
-                xPos -= v_mag*elapsedTime;
-            }                 
-            if (keys[GLFW_KEY_RIGHT]) {
-                xPos += v_mag*elapsedTime;
-            }
-
-            if (xPos - scl.x/2 > 0 && xPos + scl.x/2 < scene->SizeX) {
-                pos.x = xPos;
+                move(LEFT, elapsedTime);
+            } else if (keys[GLFW_KEY_RIGHT]) {
+                move(RIGHT, elapsedTime);
             }
         }
+
+        void move (DIRECTION direct, float elapsedTime) {
+            float   dx = PlayerVel*elapsedTime*direct;
+            float   newPos = pos.x + dx;
+            
+            if (newPos - scl.x/2 >= 0 && newPos + scl.x/2 <= scene->SizeX)
+                pos.x = newPos;
+        }
+
+        void reset () {
+            pos = glm::vec3 (scene->SizeX/2, PlayerY/2, 0.0f);
+        }
+
 };
 
-class   Ball : public PhisicObject{
+class   Ball : public Object {
 
     public:
-        Ball (Scene *sc)  {
-            scene = sc;
-            GLfloat yPos = scene->player->pos.y + scene->player->scl.y/2 + radius;
-            pos = glm::vec3 ((GLfloat)scene->SizeX/2, yPos, 0.0f);
-            scl = glm::vec3 (2*radius, 2*radius, 1.0f);
-            scene->ball = this;
-        }
-        ~Ball () {}
 
-        glm::vec3   velocity{glm::vec3(0.f,0.f,0.f)};     
-        GLfloat     radius{1.f};          
+        glm::vec3   vel{glm::vec3(VelX,VelY,0.0f)};
+        float       R{BallR};
+        float       v_mag {VelMag};
         bool        runned{false};
-        float       v_mag{400.f};
-        float       v_angle{M_PI_4};
 
-        void update (float elapsedTime) {
+        Ball (Scene *sc, Mesh *m) : Object(sc, m) {
+            pos = scene->player->pos + glm::vec3 (0, R + scene->player->scl.y/2, 0.0);
+            scl = 2.0f*glm::vec3 (R, R, 0.0);
+        }
+
+        void update (float elapsedTime)override {
             if (!runned) {
                 if (keys[GLFW_KEY_SPACE]) {
                     runned = true;
-                    velocity = glm::vec3 (v_mag*cos(v_angle), v_mag*sin(v_angle), 0.f);
-                } 
+                }
                 pos.x = scene->player->pos.x;
             } else {
                 move(elapsedTime);
             }
         }
 
-        void move (float elapsedTime) {
-            glm::vec3 tmp_pos =  pos + velocity*elapsedTime;
+        void move(float elapsedTime) {
+            glm::vec3   dx = elapsedTime*vel;
+            glm::vec3   newPos = pos + dx;
 
-            bool    bumped = false; 
-            float   dr;
-            if ((dr = tmp_pos.x-radius) < 0) {
-                velocity.x *= -1;
-                bumped = true;
-            } else if ((dr = tmp_pos.x + radius - scene->SizeX) > 0 ) {
-                velocity.x *= -1;
-                bumped = true;
+            float dr;
+            if ((dr = newPos.x - R) < 0) {
+                vel.x *= -1;
+                return;   
             }
-
-            if ((dr = tmp_pos.y + radius - scene->SizeY) > 0 ) {
-                velocity.y *= -1;
-                bumped = true;
-            } else if ((dr = tmp_pos.y - radius) < 0) {
-                // scene->player->pos.x = scene->SizeX/2;
-                // tmp_pos.x = scene->SizeX/2;
-                // tmp_pos.y = scene->player->pos.y + scene->player->scl.y/2 + radius;
-                // runned = false;
-                // bumped = true;
-                velocity.y *= -1;
-                bumped = true;
+            if ((dr = newPos.x + R - scene->SizeX) > 0) {
+                vel.x *= -1;
+                return;
             }
-
-            if (!bumped) pos = tmp_pos;
+            if ((dr = newPos.y + R -scene->SizeY) > 0) {
+                vel.y *= -1;
+                return;
+            }
+            if ((dr = newPos.y - R) < 0) {
+                scene->resetRaund();
+                return;
+            }
+            if (!(withPlayerCollision(newPos) || withBlocksCollision(newPos)))
+                pos = newPos;
         }
 
+        bool    withPlayerCollision (const glm::vec3 &newPos) {
+            glm::vec3   p1 = scene->player->pos + 0.5f*scene->player->scl;
+            glm::vec3   p2 = scene->player->pos - 0.5f*scene->player->scl;
+            p2.y += scene->player->scl.y;
+
+            if (glm::distance(p1, newPos) < R || glm::distance(p2, newPos) < R || (newPos.x < p1.x && newPos.x > p2.x && newPos.y - R < p1.y)) {
+                float   angle = M_PI*(pos.x - scene->player->pos.x) / (scene->player->scl.x + R);
+                std::cout << angle <<std::endl;
+                vel = v_mag*glm::vec3(sin(angle), cos(angle), 0.0f);
+                return true;
+            }
+
+
+            return false;
+        }
+
+        bool    withBlocksCollision (const glm::vec3 &newPos);
+
+        void reset () {
+            pos = scene->player->pos + glm::vec3 (0, R + scene->player->scl.y/2, 0.0);
+            vel = glm::vec3 (VelX, VelY, 0.0f);
+            runned = false;
+        }
+};
+
+class   Block : public Object {
+
+    public:
+
+        bool    alive{true};
+        Block (glm::vec2 p, Scene *sc, Mesh *m) : Object(sc,m) {
+            pos = glm::vec3(p, 0.0f);
+            scl = glm::vec3(BlockX, BlockY, 0.0f);
+        }
 
 };
 
-void Player::ballCollision () {
-    glm::vec3   dr = scene->ball->pos - pos;
-    float tmp_y = dr.y - (scene->ball->radius + scl.y/2);
-    if (dr.x < scl.x/2 && dr.x > -scl.x/2 && tmp_y < 0) {
-        float   angle = 2*dr.x/scl.x;
-        scene->ball->velocity = scene->ball->v_mag * glm::vec3 (sin(angle), cos(angle), 0.0f);
-        // scene->ball->velocity = scene->ball->v_mag * glm::vec3 (0.0f, 1.0f, 0.0f);
-        scene->ball->pos.y -= tmp_y;
+
+Scene::Scene (GLuint sx, GLuint sy, GLuint sh_prog, Mesh **m) : SizeX(sx), SizeY(sy), shaderProgram(sh_prog) {
+  
+    view = glm::translate (glm::mat4(1.0f), glm::vec3 (-1.f, -1.f, 0.f));
+    view = glm::scale (view, glm::vec3 (2.0f/SizeX, 2.0f/SizeY, 1.0f));
+
+    player = new Player (this, m[0]);
+    ball = new Ball (this, m[1]);
+
+    float   marginX = (SizeX - 1.1f*BlockX*BlocksCol)/2;
+    float   marginY = SizeY - 1.1f*BlockY*BlocksRow/3*4;
+
+    blockMesh = m[0];
+
+    for (int i = 0; i< BlocksRow; i++) {
+        float y = marginY + BlockY*1.1f*i;
+        for (int j = 0; j < BlocksCol; j++) {
+            float x = marginX + BlockX/2 + BlockX*1.1f*j;
+            blockList.push_back (*(new Block (glm::vec2 (x, y), this, blockMesh)));
+        }
     }
 }
-bool Block::ballCollision () {
-    Ball        *ball = scene->ball;
+void Scene::Draw() {
+    player->Draw();
+    ball->Draw();
 
-    glm::vec3   p = glm::vec3(pos.x+scl.x/2, pos.y+scl.y/2, 0.0f);
-    if (glm::distance(p, ball->pos) < ball->radius) {
-        ball->velocity *= -1;
-        ball->pos += ball->pos - p;
-        return true;
-    }
-    p = glm::vec3(pos.x+scl.x/2, pos.y-scl.y/2, 0.0f);
-    if (glm::distance(p, ball->pos) < ball->radius) {
-        ball->velocity *= -1;
-        ball->pos += ball->pos - p;
-        return true;
-    }
-    p = glm::vec3(pos.x-scl.x/2, pos.y+scl.y/2, 0.0f);
-    if (glm::distance(p, ball->pos) < ball->radius) {
-        ball->velocity *= -1;
-        ball->pos += ball->pos - p;
-        return true;
-    }
-    p = glm::vec3(pos.x-scl.x/2, pos.y-scl.y/2, 0.0f);
-    if (glm::distance(p, ball->pos) < ball->radius) {
-        ball->velocity *= -1;
-        ball->pos += ball->pos - p;
-        return true;
-    }
+    for (auto &block : blockList)
+        block.Draw();
+}
 
-    p = ball->pos;
-    if (p.x > pos.x-scl.x/2 && p.x < pos.x+scl.x/2) {
-        if (p.y+ball->radius > pos.y-scl.y/2 && p.y+ball->radius < pos.y+scl.y/2) {
-            ball->pos.y -= ball->pos.y + ball->radius - (pos.y-scl.y/2);
-            ball->velocity.y *= -1;
-            return true;
-        }
-        if (p.y-ball->radius > pos.y-scl.y/2 && p.y-ball->radius < pos.y+scl.y/2) {
-            ball->pos.y += ball->pos.y + ball->radius - (pos.y+scl.y/2);
-            ball->velocity.y *= -1;
-            return true;
-        }
-    }
-    if (p.y > pos.y-scl.y/2 && p.y < pos.y+scl.y/2) {
-        if (p.x+ball->radius > pos.x-scl.x/2 && p.x+ball->radius < pos.x+scl.x/2) {
-            ball->pos.x -= ball->pos.x + ball->radius - (pos.x-scl.x/2);
-            ball->velocity.x *= -1;
-            return true;
-        }
-        if (p.x-ball->radius > pos.x-scl.x/2 && p.x-ball->radius < pos.x+scl.x/2) {
-            ball->pos.x += ball->pos.x + ball->radius - (pos.x+scl.x/2);
-            ball->velocity.x *= -1;
-            return true;
-        }
+void Scene::update(float elapsedTime) {
+    player->update(elapsedTime);
+    ball->update(elapsedTime);
 
-        return false;
+    if (blockList.size() > 0) {
+        auto    i = std::remove_if (blockList.begin(), blockList.end(), [&](Block &o) {return !o.alive;});
+        if (i != blockList.end())
+            blockList.erase(i);
+    } else {
+        resetGame();
     }
 }
 
+void    Scene::resetRaund () {
+    player->reset();
+    ball->reset();
+}
+
+void    Scene::resetGame () {
+    resetRaund ();
+    float   marginX = (SizeX - 1.1f*BlockX*BlocksCol)/2;
+    float   marginY = SizeY - 1.1f*BlockY*BlocksRow/3*4;
+
+    for (int i = 0; i< BlocksRow; i++) {
+        float y = marginY + BlockY*1.1f*i;
+        for (int j = 0; j < BlocksCol; j++) {
+            float x = marginX + BlockX/2 + BlockX*1.1f*j;
+            blockList.push_back (*(new Block (glm::vec2 (x, y), this, blockMesh)));
+        }
+    }
+}
+
+bool    Ball::withBlocksCollision (const glm::vec3 &newPos) {
+
+    for (auto &block : scene->blockList) {
+        glm::vec3   p[] = {
+            block.pos + 0.5f*block.scl,
+            block.pos - 0.5f*block.scl,
+            block.pos + 0.5f*block.scl,
+            block.pos - 0.5f*block.scl,
+        }; 
+        p[2].y -= block.scl.y;
+        p[3].y += block.scl.y;
+        
+        for (int i = 0; i < 4; i++) {
+            if (glm::distance(p[i], pos) < R) {
+                vel *= -1.0f;
+                block.alive = false;
+                return true;
+            }
+        }
+
+        if (newPos.x < p[0].x && newPos.x > p[1].x) {
+            if ((newPos.y - R < p[0].y && newPos.y - R > p[1].y) ||
+                (newPos.y + R < p[0].y && newPos.y + R > p[1].y))  {
+                    vel.y *= -1;
+                    block.alive = false;
+                    return true;
+            }
+        } else if (newPos.y < p[0].y && newPos.y > p[1].y) {
+            if ((newPos.x - R < p[0].x && newPos.x - R > p[1].x) ||
+                (newPos.x + R < p[0].x && newPos.x + R > p[1].x))  {
+                    vel.x *= -1;
+                    block.alive = false;
+                    return true;
+            }
+        }
+        
+    }
+
+    return false;
+}
 #endif
--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@
 
 
 void    key_callback(GLFWwindow* window, int key, int scancode, int action, int mode) {
-    if(action == GLFW_PRESS)
+    if(action == GLFW_PRESS) 
         keys[key] = true;
     else if(action == GLFW_RELEASE)
         keys[key] = false;
@@ -47,65 +47,24 @@ int main (int argc, char *argv[]) {
 
 
     Shader  shader("./shaders2/default.vs", "./shaders2/default.frag");
+    Mesh    *m[] = {new Mesh ("./meshes/cube.mesh"), new Mesh ("./meshes/circle.mesh")};
+    Scene   scene (60, 60, shader.Program, m);
 
-    Scene   scene (60, 60, shader.Program);
-    Player  player (&scene);
-    Ball    ball (&scene);
-    Block   block(glm::vec2(scene.SizeX/2, scene.SizeY/2), &scene);
-
-    Mesh    cubeMesh ("./meshes/cube.mesh");
-    Mesh    circleMesh ("./meshes/circle.mesh");
-
-
-    Perfab  pPlayer (&player, &cubeMesh);
-    Perfab  pBall (&ball, &circleMesh);
-    Perfab  pBloc (&block, &cubeMesh);
     
-
     float   lastTime = glfwGetTime ();
 
     
     while (!glfwWindowShouldClose (window)) {
 
         glfwPollEvents ();
-
         float   currenTime = glfwGetTime ();
         float   elapsedTime = currenTime - lastTime;
         lastTime = currenTime;
-
-        player.update(elapsedTime);
-        ball.update (elapsedTime);
-        block.update(elapsedTime);
-        
+        scene.update(elapsedTime);
 
         glClear(GL_COLOR_BUFFER_BIT);
-
         shader.Use();
-
-
-        // glBindVertexArray (circleMesh.VAO);
-        
-        // glm::mat4   trans = glm::scale (glm::mat4(1.0f), ball.scl);
-
-        // glm::mat4   view = glm::scale (trans, glm::vec3(1.0/scene.SizeX, 1.0/scene.SizeY, 1.0));
-        // glm::mat4   rescl = glm::scale (glm::mat4(1.f), glm::vec3 (1.0f, (float)8/6, 1.0));
-
-        // GLuint      resclLoc = glGetUniformLocation (shader.Program, "rescl");
-        // glUniformMatrix4fv (resclLoc, 1, GL_FALSE, glm::value_ptr(rescl));
-
-        // GLuint     transLoc = glGetUniformLocation (shader.Program, "trans");
-        // glUniformMatrix4fv (transLoc, 1, GL_FALSE, glm::value_ptr(view));
-
-        // circleMesh.Draw();
-        // glBindVertexArray (0);
-
-        pPlayer.Draw();
-        pBall.Draw ();
-        pBloc.Draw();
-
-        pBall.Draw();
-        
-
+        scene.Draw();
         glfwSwapBuffers(window);
     }
 
